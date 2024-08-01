9b078a9fd983c2466466b964fe6c803c544b082c
Raffaele <raffaelecrocco2000@gmail.com>
2024-08-02 00:14:58 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist",
+  },
   server: {
     historyApiFallback: true,
     host: "0.0.0.0", // Listen on all network interfaces
