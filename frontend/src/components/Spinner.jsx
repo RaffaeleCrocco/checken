62d21264b104006bfdb91527703723490de73af2
Raffaele <raffaelecrocco2000@gmail.com>
2024-08-03 03:11:33 +0200

    Update Spinner.jsx

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Spinner = () => {
   return (
-    <div className="animate-ping w-16 h-16 m-8 rounded-full bg-sky-600"></div>
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="animate-spin size-8 border-[3px] border-current border-t-transparent text-gray-800 rounded-full">
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
   );
 };
 
