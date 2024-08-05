f0e3d3897c47cdcad2d353923478958f476bbfba
Raffaele <raffaelecrocco2000@gmail.com>
2024-08-05 22:10:36 +0200

    implement summary

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ import ShowCheck from "./pages/ShowCheck";
 import EditCheck from "./pages/EditCheck";
 import DeleteCheck from "./pages/DeleteCheck";
 import Dashboard from "./pages/Dashboard";
+import Summary from "./pages/Summary";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/checks/create" element={<CreateCheck />} />
+      <Route path="/summary" element={<Summary />} />
       <Route path="/dashboard/:user" element={<Dashboard />} />
       <Route path="/checks/details/:id" element={<ShowCheck />} />
       <Route path="/checks/edit/:id" element={<EditCheck />} />
