e66cb1de851f9522d53f2e251cbe705a942d5835
Raffaele <raffaelecrocco2000@gmail.com>
2024-08-05 23:55:00 +0200

    Update Summary.jsx

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -30,12 +30,99 @@ const Summary = () => {
       </p>
       {data.length > 0 ? (
         <div>
-          {data.map((user, index) => (
-            <p key={index}>{user.outs.last.amount + user.ins.last.amount}</p>
-          ))}
+          <div className="flex flex-col">
+            {data.map((user, index) => (
+              <div
+                key={index}
+                className="inline-flex items-center gap-x-2 py-3 px-4 text-sm border text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg"
+              >
+                <div className="flex items-center justify-between w-full">
+                  <div>
+                    <p className="capitalize font-semibold">{user.agent}</p>
+                    <p className="text-xs text-gray-500 w-3/4">
+                      Lo scorso mese ha realizzato {user.ins.last.number}{" "}
+                      checkin e {user.outs.last.number} checkout.
+                    </p>
+                  </div>
+                  <span>€{user.outs.last.amount + user.ins.last.amount}</span>
+                </div>
+              </div>
+            ))}
+            <div className="inline-flex items-center gap-x-2 py-3 px-4 text-sm border font-semibold bg-gray-50 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+              <div className="flex items-center justify-between w-full">
+                <div>
+                  <p className="capitalize">Totale</p>
+                  <p className="text-xs font-normal text-gray-500 w-3/4">
+                    Lo scorso mese sono stati realizzati{" "}
+                    {data.reduce((acc, user) => {
+                      return acc + user.ins.last.number;
+                    }, 0)}{" "}
+                    checkin e{" "}
+                    {data.reduce((acc, user) => {
+                      return acc + user.outs.last.number;
+                    }, 0)}{" "}
+                    checkout.
+                  </p>
+                </div>
+                <span>
+                  €
+                  {data.reduce((acc, user) => {
+                    return acc + user.outs.last.amount + user.ins.last.amount;
+                  }, 0)}
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col mt-5">
+            {data.map((user, index) => (
+              <div
+                key={index}
+                className="inline-flex items-center gap-x-2 py-3 px-4 text-sm border border-gray-800 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg"
+              >
+                <div className="flex items-center justify-between w-full">
+                  <div>
+                    <p className="capitalize font-semibold">{user.agent}</p>
+                    <p className="text-xs text-gray-500 w-3/4">
+                      Questo mese ha realizzato {user.ins.current.number}{" "}
+                      checkin e {user.outs.current.number} checkout.
+                    </p>
+                  </div>
+                  <span>
+                    €{user.outs.current.amount + user.ins.current.amount}
+                  </span>
+                </div>
+              </div>
+            ))}
+            <div className="inline-flex items-center gap-x-2 py-3 px-4 text-sm border border-gray-800 font-semibold bg-gray-50 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
+              <div className="flex items-center justify-between w-full">
+                <div>
+                  <p className="capitalize">Totale</p>
+                  <p className="text-xs font-normal text-gray-500 w-3/4">
+                    Questo mese sono stati realizzati{" "}
+                    {data.reduce((acc, user) => {
+                      return acc + user.ins.current.number;
+                    }, 0)}{" "}
+                    checkin e{" "}
+                    {data.reduce((acc, user) => {
+                      return acc + user.outs.current.number;
+                    }, 0)}{" "}
+                    checkout.
+                  </p>
+                </div>
+                <span>
+                  €
+                  {data.reduce((acc, user) => {
+                    return (
+                      acc + user.outs.current.amount + user.ins.current.amount
+                    );
+                  }, 0)}
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
       ) : (
-        <p>No data available</p>
+        "bro"
       )}
     </div>
   );
